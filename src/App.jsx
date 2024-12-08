6abf5c9b76a94c46be3f059bab4ba5299a0c7d57
Vladimir Tryasunov <tryasunov933@gmail.com>
2024-12-08 18:24:55 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Profile from "../src/components/Profile/Profile.jsx";
+import Profile from "./Components/Profile/Profile.jsx";
 import userData from "./userData.json";
-import FriendList from "../src/components/FriendList/FriendList.jsx";
+import FriendList from "./Components/FriendList/FriendList.jsx";
 import friends from "./friends.json";
-import TransactionHistory from "../src/components/TransactionHistory/TransactionHistory.jsx";
+import TransactionHistory from "./Components/TransactionHistory/TransactionHistory.jsx";
 import transactions from "./transactions.json";
 
 console.log(friends);
